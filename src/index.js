440b950807658fe3c23cdc2ee1dc1407941da855
Nora Moser <noralmoser@gmail.com>
2025-04-30 15:26:31 -0700

    added pics and a few components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Hello React!</h1>
+  return (
+  <div className='container'>
+    <Header />
+    <Menu />
+    <Footer />
+  </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className='header'>
+      <h1>Fast React Pizza Co</h1>
+    </header>
+  )
+}
+
+function Menu() {
+  return (
+    <main className='menu'>
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+    </div>
+    </main>
+  )
+}
+function Footer() {
+  const hour = new Date().getHours()
+  const openHour = 12
+  const closeHour = 22
+  const isOpen = hour >= openHour && hour <= closeHour
+
+
+  return <footer className='footer'>{new Date().toLocaleDateString()}. We're currently {isOpen ? 'Open' : 'Closed'}.</footer>
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src='pizzas/spinaci.jpg'></img>
+      <h3>Pizza</h3>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
